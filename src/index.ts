9ee0adbc97108e207a121493549d86276d44e40e
Mathis Sergent <mathis@Mathiss-MacBook-Air.local>
2025-05-26 10:50:36 +0200

    Add payment channel

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import chalk from "chalk";
-import { AccountSet, AccountSetAsfFlags, AMMCreate, AMMDeposit, Client, convertStringToHex, EscrowCreate, EscrowFinish, isoTimeToRippleTime, multisign, NFTokenBurn, NFTokenCancelOffer, NFTokenCreateOffer, NFTokenCreateOfferFlags, NFTokenMint, Payment, SignerListSet, TicketCreate, TrustSet, TrustSetFlags, xrpToDrops } from "xrpl";
+import { AccountSet, AccountSetAsfFlags, AMMCreate, AMMDeposit, Client, convertStringToHex, EscrowCreate, EscrowFinish, isoTimeToRippleTime, multisign, NFTokenBurn, NFTokenCancelOffer, NFTokenCreateOffer, NFTokenCreateOfferFlags, NFTokenMint, Payment, PaymentChannelClaim, PaymentChannelCreate, PaymentChannelFund, SignerListSet, signPaymentChannelClaim, TicketCreate, TrustSet, TrustSetFlags, xrpToDrops } from "xrpl";
 import { NFTokenCreateOfferMetadata } from "xrpl/dist/npm/models/transactions/NFTokenCreateOffer";
 import { NFTokenMintFlags, NFTokenMintMetadata } from "xrpl/dist/npm/models/transactions/NFTokenMint";
 
@@ -7,6 +7,8 @@ import crypto from 'crypto';
 
 // @ts-expect-error no types available
 import cc from 'five-bells-condition';
+import { validatePayment } from "xrpl/dist/npm/models/transactions/payment";
+import { PaymentChannelClaimFlags, validatePaymentChannelClaim } from "xrpl/dist/npm/models/transactions/paymentChannelClaim";
 
 // async function payment() {
 //     console.log(chalk.bgWhite("-- PAYMENT + MEMO --"));
@@ -347,90 +349,218 @@ import cc from 'five-bells-condition';
 //     await client.disconnect();
 // }
 
+// async function escrow() {
+//     console.log(chalk.bgWhite("-- ESCROW --"));
+//     const client = new Client("wss://s.altnet.rippletest.net:51233/");
 
+//     await client.connect();
 
-async function escrow() {
-    console.log(chalk.bgWhite("-- ESCROW --"));
-    const client = new Client("wss://s.altnet.rippletest.net:51233/");
+//     const { wallet: wallet1 } = await client.fundWallet();
+//     console.log(`Wallet 1: ${wallet1.classicAddress}`);
+
+//     const { wallet: wallet2 } = await client.fundWallet();
+//     console.log(`Wallet 2: ${wallet2.classicAddress}`);
+
+//     function generateConditionAndFulfillment() {
+//         console.log(
+//             "******* LET'S GENERATE A CRYPTO CONDITION AND FULFILLMENT *******"
+//         );
+//         console.log();
+
+//         // use cryptographically secure random bytes generation
+//         const preimage = crypto.randomBytes(32);
+
+//         const fulfillment = new cc.PreimageSha256();
+//         fulfillment.setPreimage(preimage);
+
+//         const condition = fulfillment
+//             .getConditionBinary()
+//             .toString('hex')
+//             .toUpperCase();
+//         console.log('Condition:', condition);
+
+//         // Keep secret until you want to finish the escrow
+//         const fulfillment_hex = fulfillment
+//             .serializeBinary()
+//             .toString('hex')
+//             .toUpperCase();
+
+//         console.log(
+//             'Fulfillment (keep secret until you want to finish the escrow):',
+//             fulfillment_hex
+//         );
+
+//         return { condition, fulfillment_hex };
+//     }
+
+//     const { condition, fulfillment_hex } = generateConditionAndFulfillment();
+
+//     // Create a time based escrow
+//     const escrowCreateTx: EscrowCreate = {
+//         TransactionType: "EscrowCreate",
+//         Account: wallet1.classicAddress,
+//         Destination: wallet2.classicAddress,
+//         Amount: xrpToDrops("1"),
+//         Condition: condition
+//     }
+
+//     const escrowCreateTxResult = await client.submitAndWait(escrowCreateTx, { autofill: true, wallet: wallet1 });
+//     if (escrowCreateTxResult.result.validated)
+//         console.log(`✅ EscrowCreate successful! Transaction hash: ${escrowCreateTxResult.result.hash}`);
+//     else
+//         console.log(`❌ EscrowCreate failed! Error: ${escrowCreateTxResult.result.meta}`);
+
+//     console.log(fulfillment_hex);
+
+//     // Finish the time based escrow
+//     const escrowFinishTx: EscrowFinish = {
+//         TransactionType: "EscrowFinish",
+//         Account: wallet2.classicAddress,
+//         Owner: wallet1.classicAddress,
+//         Condition: condition,
+//         Fulfillment: fulfillment_hex,
+//         OfferSequence: escrowCreateTxResult.result.tx_json.Sequence ?? 0
+//     }
+
+//     const escrowFinishTxResult = await client.submitAndWait(escrowFinishTx, { autofill: true, wallet: wallet2 });
+
+//     if (escrowFinishTxResult.result.validated)
+//         console.log(`✅ EscrowFinish successful! Transaction hash: ${escrowFinishTxResult.result.hash}`);
+//     else
+//         console.log(`❌ EscrowFinish failed! Error: ${escrowFinishTxResult.result.meta}`);
+
+//     await client.disconnect();
+// }
+
+async function channel() {
+    console.log(chalk.bgWhite("-- CHANNEL --"));
+    const client = new Client("wss://s.devnet.rippletest.net:51233/");
 
     await client.connect();
 
-    const { wallet: wallet1 } = await client.fundWallet();
-    console.log(`Wallet 1: ${wallet1.classicAddress}`);
+    const { wallet: sender } = await client.fundWallet();
+    console.log(`Sender: ${sender.classicAddress}`);
 
-    const { wallet: wallet2 } = await client.fundWallet();
-    console.log(`Wallet 2: ${wallet2.classicAddress}`);
+    const { wallet: receiver } = await client.fundWallet();
+    console.log(`Receiver: ${receiver.classicAddress}`);
 
-    function generateConditionAndFulfillment() {
-        console.log(
-            "******* LET'S GENERATE A CRYPTO CONDITION AND FULFILLMENT *******"
-        );
-        console.log();
-
-        // use cryptographically secure random bytes generation
-        const preimage = crypto.randomBytes(32);
-
-        const fulfillment = new cc.PreimageSha256();
-        fulfillment.setPreimage(preimage);
-
-        const condition = fulfillment
-            .getConditionBinary()
-            .toString('hex')
-            .toUpperCase();
-        console.log('Condition:', condition);
-
-        // Keep secret until you want to finish the escrow
-        const fulfillment_hex = fulfillment
-            .serializeBinary()
-            .toString('hex')
-            .toUpperCase();
-
-        console.log(
-            'Fulfillment (keep secret until you want to finish the escrow):',
-            fulfillment_hex
-        );
-
-        return { condition, fulfillment_hex };
-    }
-
-    const { condition, fulfillment_hex } = generateConditionAndFulfillment();
-
-    // Create a time based escrow
-    const escrowCreateTx: EscrowCreate = {
-        TransactionType: "EscrowCreate",
-        Account: wallet1.classicAddress,
-        Destination: wallet2.classicAddress,
-        Amount: xrpToDrops("1"),
-        Condition: condition,
-        FinishAfter: isoTimeToRippleTime(new Date(Date.now() + 2000))
-    }
-
-    const escrowCreateTxResult = await client.submitAndWait(escrowCreateTx, { autofill: true, wallet: wallet1 });
-    if (escrowCreateTxResult.result.validated)
-        console.log(`✅ EscrowCreate successful! Transaction hash: ${escrowCreateTxResult.result.hash}`);
-    else
-        console.log(`❌ EscrowCreate failed! Error: ${escrowCreateTxResult.result.meta}`);
-
-    console.log(fulfillment_hex);
-
-    // Finish the time based escrow
-    const escrowFinishTx: EscrowFinish = {
-        TransactionType: "EscrowFinish",
-        Account: wallet2.classicAddress,
-        Owner: wallet1.classicAddress,
-        Condition: condition,
-        Fulfillment: fulfillment_hex,
-        OfferSequence: escrowCreateTxResult.result.tx_json.Sequence ?? 0
-    }
+    // Create the channel
     
-    const escrowFinishTxResult = await client.submitAndWait(escrowFinishTx, { autofill: true, wallet: wallet2 });
+    // Example: 
+    // Day 1: I paid for 1 something... (10 XRP)
+    let amountToClaimXrp = "10";
 
-    if (escrowFinishTxResult.result.validated)
-        console.log(`✅ EscrowFinish successful! Transaction hash: ${escrowFinishTxResult.result.hash}`);
+    const channelCreateTx: PaymentChannelCreate = {
+        TransactionType: "PaymentChannelCreate",
+        Account: sender.classicAddress,
+        Destination: receiver.classicAddress,
+        Amount: xrpToDrops(amountToClaimXrp),
+        SettleDelay: 60, // Time to wait before the channel can be closed by the receiver
+        PublicKey: sender.publicKey
+    }
+
+    const channelCreateTxResult = await client.submitAndWait(channelCreateTx, { autofill: true, wallet: sender });
+
+    if (channelCreateTxResult.result.validated)
+        console.log(`✅ ChannelCreate successful! Transaction hash: ${channelCreateTxResult.result.hash}`);
     else
-        console.log(`❌ EscrowFinish failed! Error: ${escrowFinishTxResult.result.meta}`);
+        console.log(`❌ ChannelCreate failed! Error: ${channelCreateTxResult.result.meta}`);
+
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    // Retrieve the channel ID in "account_channels"
+    const channelInfo = await client.request({
+        command: "account_channels",
+        account: sender.classicAddress,
+        destination_account: receiver.classicAddress
+    });
+
+    const channelId = channelInfo.result.channels[0]?.channel_id;
+
+    if (!channelId) {
+        console.error("❌ Could not retrieve channel_id");
+        return;
+    }
+
+    console.log(`🔑 Channel ID: ${channelId}`);
+
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    let signedClaim = signPaymentChannelClaim(channelId, amountToClaimXrp, sender.privateKey);
+
+    const verifyDay1 = await client.request({
+        command: "channel_verify",
+        channel_id: channelId,
+        amount: xrpToDrops(amountToClaimXrp),
+        signature: signedClaim,
+        public_key: sender.publicKey
+    })
+
+    console.log(`🌤️ Day 1 - Signature verified: ${verifyDay1.result.signature_verified ? '✅' : '❌'}`);
+
+    // Day 2: I bought another one
+    amountToClaimXrp = "20"; // 10 + 10
+    const channelFundingTx: PaymentChannelFund = {
+        TransactionType: "PaymentChannelFund",
+        Account: sender.classicAddress,
+        Channel: channelId,
+        Amount: xrpToDrops("10") // Amount to add to the channel
+    }
+
+    const channelFundingTxResult = await client.submitAndWait(channelFundingTx, { autofill: true, wallet: sender });
+
+    if (channelFundingTxResult.result.validated)
+        console.log(`✅ ChannelFund successful! Transaction hash: ${channelFundingTxResult.result.hash}`);
+    else
+        console.log(`❌ ChannelFund failed! Error: ${channelFundingTxResult.result.meta}`);
+
+    
+    signedClaim = signPaymentChannelClaim(channelId, amountToClaimXrp, sender.privateKey);
+
+    const verifyDay2 = await client.request({
+        command: "channel_verify",
+        channel_id: channelId,
+        amount: xrpToDrops(amountToClaimXrp),
+        signature: signedClaim,
+        public_key: sender.publicKey
+    })
+
+    console.log(`🌤️ Day 2 - Signature verified: ${verifyDay2.result.signature_verified ? '✅' : '❌'}`);
+
+    // Claim the channel amount
+    const claimTx: PaymentChannelClaim = {
+        TransactionType: "PaymentChannelClaim",
+        Account: receiver.classicAddress,
+        Channel: channelId,
+        Balance: xrpToDrops(amountToClaimXrp),
+        Signature: signedClaim,
+        PublicKey: sender.publicKey
+    };
+
+    const claimResult = await client.submitAndWait(claimTx, { autofill: true, wallet: receiver });
+
+    if (claimResult.result.validated) {
+        console.log("✅ Claim settled on-chain: ", claimResult.result.hash);
+    } else {
+        console.log("❌ Failed to settle claim: ", claimResult.result.meta);
+    }
+
+    // Close the channel
+    const closePaymentChannelTx: PaymentChannelClaim = {
+        TransactionType: "PaymentChannelClaim",
+        Account: receiver.classicAddress,
+        Channel: channelId,
+        Flags: PaymentChannelClaimFlags.tfClose,
+    }
+
+    const closePaymentChannelTxResult = await client.submitAndWait(closePaymentChannelTx, { autofill: true, wallet: receiver });
+
+    if (closePaymentChannelTxResult.result.validated)
+        console.log(`✅ Channel closed! Transaction hash: ${closePaymentChannelTxResult.result.hash}`);
+    else
+        console.log(`❌ Channel close failed! Error: ${closePaymentChannelTxResult.result.meta}`);
 
     await client.disconnect();
 }
 
-escrow();
+channel();
